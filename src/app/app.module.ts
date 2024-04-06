20c51b2fb1e196b1b7e97b5254b2e51c4af98bfb
allychoung <allychoung@gmail.com>
2024-04-06 12:54:25 -0400

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentBodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content-body/content-body.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-content-body',
+  templateUrl: './content-body.component.html',
+  styleUrls: ['./content-body.component.css']
+})
+export class ContentBodyComponent {
+  @Input()
+  phase!: string;
+}
