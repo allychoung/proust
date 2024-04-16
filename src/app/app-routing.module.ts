7bbb7bb10abea101c30dbce5d379a91664fce246
allychoung <allychoung@gmail.com>
2024-04-16 12:03:08 -0400

    work on comps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { IntroComponent } from './intro/intro.component';
 import { Proj1Component } from './proj1/proj1.component';
 import { CitedComponent } from './cited/cited.component';
 import { StatementComponent } from './statement/statement.component';
+import { Proj2Component } from './proj2/proj2.component';
+import { Proj3Component } from './proj3/proj3.component';
+import { Proj4Component } from './proj4/proj4.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +22,19 @@ export const routes: Routes = [
       path: 'p1',
       component: Proj1Component
   },
+  {
+    path: 'p2',
+    component: Proj2Component
+},
+{
+  path: 'p3',
+  component: Proj3Component
+},
+{
+  path: 'p4',
+  component: Proj4Component
+},
+
   {
     path: 'cited',
     component: CitedComponent
